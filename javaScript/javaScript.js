a4eaf1557741bc4a0699190ac0864ebcf03d991a
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-10 08:51:34 +0200

    first commit

---
+++ javaScript/javaScript.js
@@ -0,0 +1,29 @@
+// QUESTO FOGLIO SERVE A RIPASSARE DEI CONCETTI BASE DELLA PROGRAMMAZIONE. 
+// IN QUANTO TALE SARA' AGGIORNATO COSTANTEMENTE E SCARICABILE GRATUITAMENTE DA TUTTE LE PERSONE INTERESSATE
+
+/*************************************************************************************************************/
+//PARTIAMO DAL PRINCIPIO
+/*************************************************************************************************************/
+ 
+//Cos'è JavaScript?
+
+//JavaScript nasce per dare dinamicità alle pagine web. 
+
+// Consente quindi di: 
+// Accedere e modificare elementi della pagina HTML 
+// Reagire ad eventi generati dall'utente 
+// Validare i dati inseriti dall'utente 
+// Interagire con il browser: 
+// - determinare il browser utilizzato 
+// - la dimensione del viewport 
+// - lavorare con i cookie
+
+// //JavaScript è «case sensitive», cioè fa distinzione tra lettere maiuscole e minuscole. 
+// Molte keyword sono costituite da parole composte unite senza alcuno spazio o trattino ad esempio: getElementById() 
+// Questo metodo di scrittura è detto camelCase, le lettere maiuscole all’interno della keyword servono per individuare facilmente l’inizio di una parola. 
+
+
+// JavaScript è un linguaggio "dinamicamente tipizzato" o "debolmente tipizzato". 
+// il che significa che il tipo di una variabile viene determinato e può cambiare in fase di esecuzione
+// Questa flessibilità può portare a maggiore velocità di sviluppo ma anche a errori di tipo difficili da individuare. 
+
