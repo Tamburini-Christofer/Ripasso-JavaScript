c688c6ba271c67394772f17fb232185f79f249d3
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-10 09:19:45 +0200

    Variabile: Fondamenta della programmazione

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -39,7 +39,7 @@
 // ad esempio un'operazione potrebbe restituire la stringa "5" invece del numero 5. 
 
 /*************************************************************************************************************/
-//VARIABILI
+//VARIABILI: COS'E' UNA VARIABILE
 /*************************************************************************************************************/
 
 // La variabile è un contenitore!!
@@ -113,4 +113,97 @@ console.log(persona.eta); // 31
 // - In generale non devono essere presenti caratteri diversi da lettere, numeri o underscore (_) 
 // - I nomi devono essere diversi dalle keyword riservate di JavaScript 
 
+/*************************************************************************************************************/
+//VARIABILI: FONDAMENTA DELLA PROGRAMMAZIONE
+/*************************************************************************************************************/
+
+// Ci sono 3 azioni che caratterizzano la definizione di una variabile: 
+
+// 1. Dichiarazione:
+// Questa è la prima fase in cui si "crea" la variabile, specificando il suo nome (un identificatore univoco) e il suo tipo (ad esempio, intero, stringa, booleano), 
+//      che determina il tipo di dati che può contenere e le operazioni possibili. 
+
+// 2. Allocazione:
+// Dopo la dichiarazione, viene assegnata alla variabile un'area di memoria in cui il suo valore sarà memorizzato. 
+//      È come riservare uno spazio per la "scatola" che rappresenterà la variabile. 
+
+// 3. Assegnazione:
+// Questa azione consiste nel dare un valore alla variabile. 
+//      È il processo di scrittura del dato all'interno dell'area di memoria allocata, utilizzando un operatore di assegnazione,
+//      che spesso è rappresentato dal simbolo "=". 
+
+// Quando si crea una variabile spesso contemporaneamente le si assegna un valore, per mezzo del segno di "uguale". 
+// In questo modo stiamo conservando un dato che potremo riutilizzare nel nostro codice. 
+// È anche possibile creare una variabile "vuota" e poi assegnarle un valore in un secondo momento con un'istruzione di assegnazione.
+
+//LE CONSTANTI NON SONO RIASSEGNABILI IN UN SECONDO MOMENTO
+
+// Variabili riassegnabili 
+
+// Una variabile inizializzata con la keyword let è riassegnabile in futuro. 
+
+// Quando si riassegna un valore a una variabile, non bisogna ripetere let 
+// Una variabile con la keyword let può essere inizializzata senza valore e ricevere un valore 
+
+/*************************************************************************************************************/
+//VARIABILI: TIPI DI DATO
+/*************************************************************************************************************/
+
+// l codice JavaScript manipola dei valori ed ogni valore appartiene ad un Tipo. 
+
+// I Tipi JavaScript sono: 
+
+// - Number - Numero 
+    const numero = 0;
+    // Le espressioni sono combinazioni di valori, variabili e operatori che JavaScript valuta e risolve in un singolo valore. 
+
+    // Alcuni tipi tipi di espressione in JavaScript: 
+    //     - espressioni aritmetiche: utilizzano operatori matematici per calcolare un valore numerico, ad esempio 5 * 10 o a - b 
+    //     - espressioni di stringa: combinano stringhe usando l'operatore +, 
+    //         come in 'Hello' + ' ' + 'World!'    
+    //      - espressioni logiche: utilizzano operatori logici per valutare condizioni, ad esempio x > 5 && y < 10 (che vedremo più avanti)
+
+// - String - Stringa, sequenza di caratteri 
+    const stringa = "ciao"
+    // Le stringhe possono essere unite con il segno +. Questa operazione si chiama concatenazione ed il + prende il nome di operatore di concatenazione. 
+
+    // Esiste un'altra modalità per concatenare le stringhe e interpolare valori senza troppa fatica. 
+    //     1. Utilizziamo i backtick al posto delle virgolette. 
+    //     2. Per incorporare una variabile nella nostra stringa usiamo la sintassi (interpolazione) ${nomeVariabile} 
+
+// - Boolean - true|false, 1|0 
+    const vero = true;
+
+// - Object - Oggetto 
+    const oggetto = {};
+
+// - Function - Funzione, un Oggetto «speciale» 
+    function funzione () {};
+
+// - Undefined - Non Definito 
+
+/*************************************************************************************************************/
+//VARIABILI: PROPRIETA' DEI DATI
+/*************************************************************************************************************/
+
+// Accedere alle proprietà:
+
+// Ogni tipo di dato in JavaScript ti dà accesso a delle proprietà (attributi e metodi) utili. 
+
+// Per accedere a queste proprietà basta aggiungere un punto . davanti al tipo di dato. 
+
+// Alcune di queste proprietà rappresentano delle funzioni (che vedremo più avanti) e come tali vanno invocate tramite parentesi tonde () alla fine. 
+
+// Ad esempio, un paio di proprietà utili per i numeri sono .toFixed() e .toString() 
+
+// toString()
+    let numeroA = 123;
+    let stringaNumero = numero.toString();
+    console.log(stringaNumero); // Output: "123"
+
+//toFixed()
+    let numeroOriginale = 10.123456;
+    let numeroFormattato = numeroOriginale.toFixed(2); // Arrotonda a 2 cifre decimali
+
+    console.log(numeroFormattato); // Stampa: "10.12"
 
